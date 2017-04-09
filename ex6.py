dd95f4f1f6d242e969fcf775d7047d568fbc2a2b
root <root@no-ptr.123host.vn>
2017-04-09 03:41:38 -0400

    first commit

---
+++ ex6.py
@@ -0,0 +1,29 @@
+#!/bin/env python
+
+# Khai bao chuoi
+var1 = 'Hello World'
+var2 = 'Python Programming'
+
+#In ra cacs phan tu trong chuoi
+print "var1[1]: ", var1[1]
+print "var2[0:6]: ", var2[0:6]
+print "Chuoi moi la: ", var1[:6] + 'Python'
+
+a = "Tong Duy Ngoc... "
+b = "Nhan vien ky thuat"
+
+print a + b
+print "Toi ten la %s va toi nang %d kg! WTF Toi rat om!!" %('Ngoc', 50)
+
+# Demo tripple quote
+c = """Tong Duy Ngoc
+Nhan vien Ky thuat
+123HOST"""
+
+print c 
+
+d = 'Tong'
+
+d in a
+
+
